7f3a2304ea868ff55ea6d081e425f5c06c0ba837
Luendel <luendel3@hotmail.com>
2024-10-25 20:44:08 -0300

    modified:   src/app/components/heroImage.component.html modified:   src/app/components/heroImage.component.ts

--- src/app/components/heroImage.component.ts
+++ src/app/components/heroImage.component.ts
@@ -1,10 +1,56 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, AfterViewInit, OnInit} from "@angular/core";
+import { Subscription, fromEvent } from "rxjs"
 
 @Component({
     selector:"app-hero-image",
     templateUrl:"./heroImage.component.html",
     styleUrl:"./heroImage.component.css"
 })
-export class HeroImage {
+export class HeroImage implements AfterViewInit, OnDestroy, OnInit {
+
+    path!: string;
+
+    resizeSubscription!: Subscription;
+    loadSubscription!: Subscription;
+
+    constructor(){}
+    
+    ngOnInit(): void {
+            this.loadSubscription = fromEvent(window,"load")
+                .subscribe(()=> {
+                    if(window.innerWidth >= 750 && window.innerWidth - window.innerHeight >= 320){
+                        this.path = "images/hero-desktop.jpg"
+
+                    } else {
+                        this.path = "images/hero-mobile.jpg";
+                    }
+                })
+        }
+    
+    ngAfterViewInit(): void {
+            this.resizeSubscription = fromEvent(window , "resize")
+            .subscribe(()=> {
+                if(window.innerWidth >= 750 && window.innerWidth - window.innerHeight >= 320){
+                    this.path = "images/hero-desktop.jpg"
+
+                } else {
+                    this.path = "images/hero-mobile.jpg";
+                }
+            })
+
+            
+    }
+
+   
+
+    ngOnDestroy(): void {
+        if(this.resizeSubscription){
+            this.resizeSubscription.unsubscribe()
+        }
+        if(this.loadSubscription){
+            this.loadSubscription.unsubscribe()
+        }
+        
+    }
     
 }
